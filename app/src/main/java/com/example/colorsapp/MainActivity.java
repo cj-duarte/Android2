59b2bab3c8ad97fea5065e4bdba5b3bcf121b594
cj-duarte <cj-duarte@hotmail.com>
2019-10-08 22:14:55 -0500

    actualizacion de archivos

---
+++ app/src/main/java/com/example/colorsapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.colorsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    //1. Incializar componentes a Null
+    private SeekBar sbrRed = null;
+    private SeekBar sbrGreen = null;
+    private SeekBar sbrBlue = null;
+    private SeekBar sbrAlpha = null;
+    private View vieColors = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //2. Obtener componentes ids
+        sbrRed = findViewById(R.id.sbrRed);
+        sbrGreen = findViewById(R.id.sbrGreen);
+        sbrBlue = findViewById(R.id.sbrBlue);
+        sbrAlpha = findViewById(R.id.sbrAlpha);
+        vieColors = findViewById(R.id.vieColors);
+
+        //3. Habilitar View Component como ContextMenu.
+        registerForContextMenu(vieColors);
+
+        //4. Obtener el progreso de los SeekBar => Valor
+        sbrRed.setOnSeekBarChangeListener(this);
+        sbrGreen.setOnSeekBarChangeListener(this);
+        sbrBlue.setOnSeekBarChangeListener(this);
+        sbrAlpha.setOnSeekBarChangeListener(this);
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean bo) {
+        //5. Cambiar colores
+        int r = sbrRed.getProgress();
+        int g = sbrGreen.getProgress();
+        int b = sbrBlue.getProgress();
+        int a = sbrAlpha.getProgress();
+
+        int colorHex = Color.argb(a,r,g,b);
+        vieColors.setBackgroundColor(colorHex);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+}
